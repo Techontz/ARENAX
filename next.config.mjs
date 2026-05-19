4773b18892e34614bc8a697648823bf6095056b1
techontz <techontz@gmail.com>
2026-05-19 09:52:58 +0300

    Allow sofascore images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Config options here
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname:
+          'img.sofascore.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
